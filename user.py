c5c6a01aecb4f421885e47b5fb3dcdc4a507e5f4
akakou <akakou571@gmail.com>
2020-05-11 15:09:38 +0900

    ユーザに関するオブジェクトを追加

---
+++ user.py
@@ -0,0 +1,42 @@
+class GameUser:
+    def __init__(self, member):
+        self.member = member
+        self.channel = None
+        self.vote = None
+        self.spoofed = None
+        self.is_answer = False
+
+class GameUserRepository():
+    users = []
+
+    def add(self, user):
+        self.users.append(user)
+    
+    def update(self, user):
+        for index, _user in enumerate(self.users):
+            if _user.member == user.member:
+                self.users[index] = user
+
+    def get(self, name='', member=None, channel=None, vote=None, spoofed=None, is_answer=False):
+        for user in self.users:
+            if name and user.member.name != name:
+                continue
+
+            if member and user.member != member:
+                continue
+
+            if channel and user.channel != channel:
+                continue
+
+            if vote and user.vote != vote:
+                continue
+
+            if spoofed and user.spoofed != spoofed:
+                continue
+
+            if is_answer and not user.is_answer:
+                continue
+
+            return user
+        
+        return None
